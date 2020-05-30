df03adf1d659b2ab99c8ffdecd1c7d69999bc9b3
Mostafa M. Mohamed <mostafa.amin93@gmail.com>
2020-05-30 10:56:42 +0200

    error analysis cleanup

--- common/error_analysis.py
+++ common/error_analysis.py
@@ -4,6 +4,7 @@ import random
 
 from utils.multiviewer import MultiViewer
 from utils.logger import logger
+from utils.tolerant_comparer import print_comparison, is_correct_tolerant
 
 
 BENCHMARKS_ENUM = ['0_0.1', '0_1', '0.1_0.1', '0.1_1', '0.1_inf', '0_inf']
@@ -14,7 +15,7 @@ MODELS_ENUM = [
     'two_pass']
 
 
-def read_triples(model, benchmark, evaluation_set='test', shuffle=False):
+def read_quads(model, benchmark, evaluation_set='test', shuffle=False):
     assert model in MODELS_ENUM, 'model %s not in %s' % (model, MODELS_ENUM)
     assert benchmark in BENCHMARKS_ENUM, 'benchmark %s not in %s' % (benchmark, BENCHMARKS_ENUM)
     fixed = os.path.join(ROOT_PATH, 'results_sentences', benchmark,
@@ -23,31 +24,37 @@ def read_triples(model, benchmark, evaluation_set='test', shuffle=False):
                            evaluation_set, 'correct.txt')
     corrupt = os.path.join(ROOT_PATH, 'benchmarks_sentences', benchmark,
                            evaluation_set, 'corrupt.txt')
-    with open(fixed, 'r') as fixed_file:
-        with open(correct, 'r') as correct_file:
-            with open(corrupt, 'r') as corrupt_file:
-                triples = list(zip(correct_file, corrupt_file, fixed_file))
+    original = os.path.join(ROOT_PATH, 'sentences', 'test.txt')
+    with open(original, 'r') as original_file:
+        with open(fixed, 'r') as fixed_file:
+            with open(correct, 'r') as correct_file:
+                with open(corrupt, 'r') as corrupt_file:
+                    quads = list(zip(original_file, correct_file, corrupt_file, fixed_file))
     if shuffle:
         random.seed(41)
-        random.shuffle(triples)
-    return triples
+        random.shuffle(quads)
+    return quads
+
 
 
 if __name__ == '__main__':
-    model = 'two_pass'
+    model = 'beam_search_labeling'
     benchmark = '0_0.1'
-    take_first_n = 100
     comparator = MultiViewer()
+    take_first_n = 50
+    count = 0
 
-    for correct, corrupt, fixed in read_triples(model, benchmark):
-        if correct == fixed:
+    for original, correct, corrupt, fixed in read_quads(model, benchmark):
+        if correct == fixed or is_correct_tolerant(original, correct, corrupt, fixed):
             continue
-        if take_first_n < 1:
-            break
+        take_first_n -= 1
+        if take_first_n < 0: break
+        count += 1
         metrics, out = comparator.evaluate(correct, corrupt, fixed)
         accuracy = metrics[-1]
         assert accuracy == 0
         logger.log_info(model, benchmark, highlight=2)
+        logger.output(original)
         logger.output(out)
+        logger.output(count, 'errors so far..')
         logger.log_seperator()
-        take_first_n -= 1
