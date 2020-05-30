08b5055e39b1e63b2852693717b89cbb1d51b511
Mostafa M. Mohamed <mostafa.amin93@gmail.com>
2020-05-30 10:40:53 +0200

    multiple error comparison cleanup

--- common/error_multiple_comparison.py
+++ common/error_multiple_comparison.py
@@ -42,54 +42,66 @@ def get_original_sentences():
     return lines
 
 
-def compare_two(original, data_a, data_b, count, better_model):
+def compare_two(original, data_a, data_b, count):
     correct_a, corrupt_a, fixed_a, benchmark_a, model_a = data_a
     correct_b, corrupt_b, fixed_b, benchmark_b, model_b = data_b
 
     metrics_a, out_a = comparator.evaluate(correct_a, corrupt_a, fixed_a)
     metrics_b, out_b = comparator.evaluate(correct_b, corrupt_b, fixed_b)
+    if metrics_a[-1] == metrics_b[-1]:
+        return 0
+    logger.output(original)
+    logger.log_info(model_b, benchmark_b, highlight=3)
+    logger.output(out_b)
+
     logger.output(original)
     logger.log_info(model_a, benchmark_a, highlight=2)
     logger.output(out_a)
-    logger.log_info(model_b, benchmark_b, highlight=3)
-    logger.output(out_b)
-    logger.log_info(better_model, 'is better', highlight=5)
+
+    logger.log_info(model_a, 'is better', highlight=5)
     logger.log_info(count, 'errors so far')
     logger.log_seperator()
+    logger.log_seperator()
+    return 1
 
 
 if __name__ == '__main__':
-    model_a = 'two_pass'
-    model_b = 'two_pass_robust'
-    benchmark_a = '0.1_1'  #'0_0.1'
+    model_a = 'two_pass_robust'
+    model_b = 'beam_search_labeling'
+    benchmark_a = '0.1_0.1'
     benchmark_b = '0_0.1'
-    count = 0
-    a, b = 0, 0
-    original_sentences = get_original_sentences()
 
+    original_sentences = get_original_sentences()
     comparator = MultiViewer()
-    i = 0
-    count = 0
-    for (correct_a, corrupt_a, fixed_a), (correct_b, corrupt_b, fixed_b) in zip(read_triples(model_a, benchmark_a), read_triples(model_b, benchmark_b)):
-        original = original_sentences[i]
-        i += 1
-        # A is better
-        if not (is_correct_tolerant(original, correct_a, corrupt_a, fixed_a) and
-                not is_correct_tolerant(original, correct_b, corrupt_b, fixed_b)):
-            continue
-        count += 1
-        data_a = (correct_a, corrupt_a, fixed_a, benchmark_a, model_a)
-        data_b = (correct_b, corrupt_b, fixed_b, benchmark_b, model_b)
-        compare_two(original, data_a, data_b, count, model_a)
-    i = 0
-    count = 0
-    for (correct_a, corrupt_a, fixed_a), (correct_b, corrupt_b, fixed_b) in zip(read_triples(model_a, benchmark_a), read_triples(model_b, benchmark_b)):
-        original = original_sentences[i]
-        i += 1
-        if not (not is_correct_tolerant(original, correct_a, corrupt_a, fixed_a) and
+
+    a, b = 0, 0
+    for original, (correct_a, corrupt_a, fixed_a), (correct_b, corrupt_b, fixed_b) in zip(
+            original_sentences, read_triples(model_a, benchmark_a), read_triples(model_b, benchmark_b)):
+        if (not is_correct_tolerant(original, correct_a, corrupt_a, fixed_a) and
                 is_correct_tolerant(original, correct_b, corrupt_b, fixed_b)):
-            continue
-        count += 1
-        data_a = (correct_a, corrupt_a, fixed_a, benchmark_a, model_a)
-        data_b = (correct_b, corrupt_b, fixed_b, benchmark_b, model_b)
-        compare_two(original, data_a, data_b, count, model_b)
+            b += 1
+        if (is_correct_tolerant(original, correct_a, corrupt_a, fixed_a) and not
+                is_correct_tolerant(original, correct_b, corrupt_b, fixed_b)):
+            a += 1
+    logger.log_info("%d where %s is better, %d where %s is better" % (
+        a, model_a, b, model_b), highlight=1)
+
+    count = 0
+    for original, (correct_a, corrupt_a, fixed_a), (correct_b, corrupt_b, fixed_b) in zip(
+            original_sentences, read_triples(model_a, benchmark_a), read_triples(model_b, benchmark_b)):
+        # B is better, A is bad
+        if (not is_correct_tolerant(original, correct_a, corrupt_a, fixed_a) and
+                is_correct_tolerant(original, correct_b, corrupt_b, fixed_b)):
+            data_a = (correct_a, corrupt_a, fixed_a, benchmark_a, model_a)
+            data_b = (correct_b, corrupt_b, fixed_b, benchmark_b, model_b)
+            count += compare_two(original, data_b, data_a, count)
+    count = 0
+    for original, (correct_a, corrupt_a, fixed_a), (correct_b, corrupt_b, fixed_b) in zip(
+            original_sentences, read_triples(model_a, benchmark_a), read_triples(model_b, benchmark_b)):
+        i += 1
+        # A is better, B is bad
+        if (is_correct_tolerant(original, correct_a, corrupt_a, fixed_a) and not
+                is_correct_tolerant(original, correct_b, corrupt_b, fixed_b)):
+            data_a = (correct_a, corrupt_a, fixed_a, benchmark_a, model_a)
+            data_b = (correct_b, corrupt_b, fixed_b, benchmark_b, model_b)
+            count += compare_two(original, data_a, data_b, count)
