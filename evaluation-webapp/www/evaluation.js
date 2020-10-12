15c46a41207eff841d63f0b08a0d7ac387b0b20f
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2020-10-12 17:53:13 +0200

    evaluation webapp

---
+++ evaluation-webapp/www/evaluation.js
@@ -0,0 +1,206 @@
+$(document).ready(function() {
+    console.log("document ready");
+    
+    // GET BENCHMARKS
+    
+    benchmarks = [];
+    
+    $.get("../benchmarks", function(data) {
+        $(data).find("a").each(function() {
+            name = $(this).attr("href");
+            if (!name.endsWith(".txt")) {
+                name = name.substring(0, name.length - 1);
+                console.log(name);
+                benchmarks.push(name);
+                $("#select_benchmark").append(new Option(name, name));
+            }
+        });
+        set_prediction_options()
+    });
+    
+    // GET PREDICTION FILES
+    
+    $("#select_benchmark").change(function() {
+        set_prediction_options();
+    })
+    
+    $("#select_subset").change(function() {
+        set_prediction_options();
+    })
+    
+    $("#select_predictions").change(function() {
+        create_table();
+    })
+});
+
+function set_prediction_options() {
+    benchmark = $("#select_benchmark option:selected").val();
+    console.log(benchmark);
+    
+    subset = $("#select_subset option:selected").val();
+    console.log(subset);
+    
+    prediction_files = [];
+    $("#select_predictions").empty();
+    
+    read_benchmark();
+    
+    results_dir = "../results/" + benchmark + "/" + subset + "/";
+    $.get(results_dir, function(data) {
+        $(data).find("a").each(function() {
+            name = $(this).attr("href");
+            prediction_files.push(name);
+            $("#select_predictions").append(new Option(name, name));
+        });
+        create_table();
+    });
+}
+
+function read_benchmark() {
+    benchmark_dir = "../benchmarks/" + benchmark + "/" + subset + "/";
+    $.get(benchmark_dir + "correct.txt", function(data) {
+        correct_sequences = []
+        for (sequence of data.split("\n")) {
+            if (sequence.length > 0) {
+                correct_sequences.push(sequence);
+            }
+        }
+    });
+    $.get(benchmark_dir + "corrupt.txt", function(data) {
+        corrupt_sequences = data.split("\n");
+    });
+}
+
+function create_table() {
+    $("#table").html("evaluating...");
+	
+    selected = $("#select_predictions option:selected").val();
+    predictions_file = results_dir + selected;
+    console.log(predictions_file);
+    
+    $.get(predictions_file, function(data) {
+        predicted_sequences = data.split("\n");
+        
+        n = Math.min(correct_sequences.length, corrupt_sequences.length, predicted_sequences.length);
+        console.log(n);
+        
+        n_corrupt = 0;
+        n_correct = 0;
+        
+        n_tp = 0;
+        n_fp = 0;
+        n_fn = 0;
+        
+        table = "<table>\n";
+        table += "<th>ID</th>"
+        table += "<th>INPUT</th>"
+        table += "<th>GROUND TRUTH</th>"
+        table += "<th>PREDICTED</th>"
+        table += "<th>TP</TP>"
+        table += "<th>FP</TP>"
+        table += "<th>FN</TP>\n"
+        
+        for (var i = 0; i < n; i++) {
+            // ground truth and prediction
+            
+            [ground_truth, highlight_true] = get_differences(corrupt_sequences[i], correct_sequences[i]);
+            correct_highlighted = highlight_positions(correct_sequences[i], highlight_true);
+            [predictions, highlight_predicted] = get_differences(corrupt_sequences[i], predicted_sequences[i]);
+            predicted_highlighted = highlight_positions(predicted_sequences[i], highlight_predicted);
+            
+            is_corrupted = correct_sequences[i] != corrupt_sequences[i];
+            is_correct = predicted_sequences[i] == correct_sequences[i];
+            
+            // evaluate TP, FP, FN
+            
+            tp = ground_truth.filter(x => predictions.includes(x));
+            fp = predictions.filter(x => !ground_truth.includes(x));
+            fn = ground_truth.filter(x => !predictions.includes(x));
+            
+            n_tp += tp.length;
+            n_fp += fp.length;
+            n_fn += fn.length;
+            
+            if (is_corrupted) {
+                n_corrupt += 1;
+            }
+            if (is_correct) {
+                n_correct += 1;
+            }
+            
+            // colors
+            
+            corrupt_color = "black";
+            predicted_color = "black";
+            if (is_corrupted) {
+                corrupt_color = "red";
+                if (is_correct) {
+                	predicted_color = "green";
+                }
+            }
+            if (!is_correct) {
+            	predicted_color = "red";
+            }
+            
+            // row
+            // .. sequences
+            row = "<tr>";
+            row += "<td>" + i + "</td>";
+            row += "<td style=\"color:" + corrupt_color + "\">" + corrupt_sequences[i] + "</td>";
+            row += "<td>" + correct_highlighted + "</td>";
+            row += "<td style=\"color:" + predicted_color + "\">" + predicted_highlighted + "</td>";
+            // .. evaluation counts
+            row += "<td>" + tp.length + "</td>";
+            row += "<td>" + fp.length + "</td>";
+            row += "<td>" + fn.length + "</td>";
+            row += "</tr>";
+            table += row + "\n";
+        }
+        table += "</table>";
+        
+        precision = n_tp / (n_tp + n_fp);
+        recall = n_tp / (n_tp + n_fn);
+        f1 = 2 * precision * recall / (precision + recall);
+        
+        evaluation = "corrupt sequences: " + (n_corrupt / n) + " (" + n_corrupt + "/" + n + ")<br>\n";
+        evaluation += "sequence accuracy: " + (n_correct / n) + " (" + n_correct + "/" + n + ")<br>\n";
+        evaluation += "true positives: &nbsp;" + n_tp + "<br>\n";
+        evaluation += "false positives: " + n_fp + "<br>\n";
+        evaluation += "false negatives: " + n_fn + "<br>\n";
+        evaluation += "precision: " + precision.toFixed(4) + "<br>\n";
+        evaluation += "recall: &nbsp;&nbsp;&nbsp;" + recall.toFixed(4) + "<br>\n";
+        evaluation += "F1-score: &nbsp;" + f1.toFixed(4) + "<br>\n";
+        evaluation += "<br>\n" + table;
+        
+        $("#table").html(evaluation);
+    });
+}
+
+function get_differences(a, b) {
+    diff_positions_a = [];
+    diff_positions_b = [];
+    var i = 0;
+    var j = 0;
+    while (i < a.length && j < b.length) {
+        if (a[i] == b[j]) {
+            i += 1;
+            j += 1;
+        } else {
+            diff_positions_a.push(i);
+            diff_positions_b.push(j);
+            if (a[i] == " ") {
+                i += 1;
+            } else {
+                j += 1;
+            } 
+        }
+    }
+    return [diff_positions_a, diff_positions_b];
+}
+
+function highlight_positions(text, positions) {
+    for (pos of positions.reverse()) {
+        text = text.substring(0, pos) + "<u>" + text[pos] + "</u>" + text.substring(pos + 1);
+    }
+    return text;
+}
