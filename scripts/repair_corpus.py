94aaa70f792ef1dd6a84126ac2cf12d0e8dd094c
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2021-06-10 19:54:13 +0200

    repair corpus: attach lines when one ends with a dash (and repair lines individually otherwise)

--- scripts/repair_corpus.py
+++ scripts/repair_corpus.py
@@ -3,6 +3,8 @@ import random
 import math
 import sys
 
+import numpy as np
+
 import project
 from src.models.char_lm.unidirectional_model import UnidirectionalModel
 from src.estimator.bidirectional_labeling_estimator import BidirectionalLabelingEstimator
@@ -36,43 +38,35 @@ if __name__ == "__main__":
         add_epsilon=True
     )
 
-    out_dir = in_dir[:-1] + ".repaired_three/"
+    out_dir = in_dir[:-1] + ".repaired_hyphens/"
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
 
     for file in files:
         print("** " + file + " **")
-        sequences = read_lines(in_dir + file)[:5]
+        sequences = read_lines(in_dir + file)
         sequences = [s.strip() for s in sequences]
         repaired_sequences = []
-        for i in range(len(sequences)):
-            start = i  # max(0, i - 1)
-            end = i + 1
-            paragraph = ""
-            undeletable_space_positions = set()
-            for paragraph_sequence in sequences[start:(end + 1)]:
-                if len(paragraph) > 0 and not paragraph.endswith("-"):
-                    undeletable_space_positions.add(len(paragraph.replace(" ", "")))
-                    paragraph += " "
-                paragraph += paragraph_sequence
-            predicted = corrector.correct(paragraph, undeletable_space_positions=undeletable_space_positions)
-            prefix_len_no_spaces = 0 if start == i else len(sequences[i - 1].replace(" ", ""))
-            seq_len_no_spaces = len(sequences[i].replace(" ", ""))
-            c_no_space_chars = 0
-            for j, char in enumerate(predicted):
-                if c_no_space_chars == prefix_len_no_spaces:
-                    span_start = j
-                if char != " ":
-                    c_no_space_chars += 1
-                if c_no_space_chars == prefix_len_no_spaces + seq_len_no_spaces:
-                    span_end = j + 1
-                    break
-            print(undeletable_space_positions)
-            predicted_sequence = predicted[span_start:span_end]
-            if sequences[i] != predicted_sequence:
-                print(sequences[i])
-                print(paragraph)
-                print(predicted)
-                print(predicted_sequence)
-            repaired_sequences.append(predicted_sequence)
+        i = 0
+        while i < len(sequences):
+            batch_sequences = [sequences[i]]
+            while batch_sequences[-1][-1] == "-" and i + 1 < len(sequences):
+                i += 1
+                batch_sequences.append(sequences[i])
+            i += 1
+            batch = "".join(batch_sequences)
+            predicted = corrector.correct(batch)
+            if len(batch_sequences) == 1:
+                repaired_sequences.append(predicted)
+            else:
+                split_positions = set(np.cumsum([len(seq.replace(" ", "")) for seq in batch_sequences]))
+                start = 0
+                nospace_chars = 0
+                for pos in range(len(predicted)):
+                    if predicted[pos] != " ":
+                        nospace_chars += 1
+                        if nospace_chars in split_positions:
+                            seq = predicted[start:(pos + 1)].strip()
+                            repaired_sequences.append(seq)
+                            start = pos + 1
         write_lines(out_dir + file, repaired_sequences)
